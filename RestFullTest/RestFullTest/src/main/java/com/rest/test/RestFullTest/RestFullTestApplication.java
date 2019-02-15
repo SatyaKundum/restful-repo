07781eef81252253799d8636a1846f6280e2822a
SatyaKundum <SatyaKundum@gmail.com>
2019-02-15 07:37:44 +0530

    RestFullService

---
+++ RestFullTest/RestFullTest/src/main/java/com/rest/test/RestFullTest/RestFullTestApplication.java
@@ -0,0 +1,14 @@
+package com.rest.test.RestFullTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullTestApplication.class, args);
+	}
+
+}
+
---
+++ RestFullTest/RestFullTest/src/test/java/com/rest/test/RestFullTest/RestFullTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rest.test.RestFullTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestFullTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
