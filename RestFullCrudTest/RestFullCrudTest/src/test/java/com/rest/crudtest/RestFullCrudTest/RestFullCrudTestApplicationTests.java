f07adf40d7130d41d6c12439cfabc7b503692ab8
SatyaKundum <SatyaKundum@gmail.com>
2019-02-15 18:33:37 +0530

    Added Junit Test Cases

--- RestFullCrudTest/RestFullCrudTest/src/test/java/com/rest/crudtest/RestFullCrudTest/RestFullCrudTestApplicationTests.java
+++ RestFullCrudTest/RestFullCrudTest/src/test/java/com/rest/crudtest/RestFullCrudTest/RestFullCrudTestApplicationTests.java
@@ -1,17 +1,50 @@
 package com.rest.crudtest.RestFullCrudTest;
 
+
+import java.util.List;
+
+import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RestFullCrudTestApplicationTests {
+import model.Customer;
 
+
+public class RestFullCrudTestApplicationTests extends Assert{
+
+	RestTemplate template = new RestTemplate();
+	
+	private static final String BASE_URI = "http://localhost:9020/customers";
+	
 	@Test
-	public void contextLoads() {
+	public void test_get_all_customers_getForObject() {
+	 List list = template.getForObject(BASE_URI, List.class);
+	 assertNotNull(list);
 	}
+	
+	@Test
+	public void test_get_all_customers_getForEntiry() {
+	 ResponseEntity<List> response =  template.getForEntity(BASE_URI, List.class);
+	 assertEquals(response.getStatusCode().value(), 200);
+	}
+	
+	@Test(expected=HttpClientErrorException.class)
+	public void test_delete_customer_exception() {
+		ResponseEntity<String> response = template.exchange("http://localhost:9020/customers/100", HttpMethod.DELETE, null, String.class);
+		assertTrue(response.getStatusCode().is2xxSuccessful());
+	}
+	
+	@Test(expected=HttpClientErrorException.class)
+	public void test_delete_customer_success() {
+		template.delete("http://localhost:9020/customers/3");
+		ResponseEntity<Customer> response = template.getForEntity("http://localhost:9020/customers/3", Customer.class);
+		assertTrue(response.getStatusCode().is2xxSuccessful());
+	}
+	
+	
 
 }
 
