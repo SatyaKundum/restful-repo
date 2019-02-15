35d7ff1ee8ebe7096c6e35824fd65670ef0130f9
SatyaKundum <SatyaKundum@gmail.com>
2019-02-15 14:07:40 +0530

    Updated Rest calls

--- RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/CustomerController.java
+++ RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/CustomerController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +27,40 @@ public class CustomerController {
 	@ResponseBody
 	public List<Customer> getAllCustomers(){
 		return list;
+	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	@ResponseBody
+	public Customer getCustomer(@PathVariable("id") Long id) {
+		for (Customer c : list) {
+			if(c.getId().equals(id)) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	@RequestMapping(value="", method=RequestMethod.POST)
+	@ResponseBody
+	public void addCustomer(@RequestBody Customer customer) {
+		list.add(customer);
+	}
+	
+	@RequestMapping(value= "/{id}", method=RequestMethod.DELETE)
+	public String deleteCustomer(@PathVariable("id") Long id) {
+		String message = "";
+		Customer customer = null;
+		for (Customer c : list) {
+			if(c.getId().equals(id)) {
+				customer = c;
+			}
+		}
+		if(customer == null) {
+			message = "Customer Not Found";
+		}else {
+			list.remove(customer);
+		}
+		return message;
 	}
 
 }
--- RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/HelloController.java
+++ RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/HelloController.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import java.awt.PageAttributes.MediaType;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
