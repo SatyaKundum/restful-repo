19a7b50fa368d2efb8c7b39de1e5fce001ddb906
SatyaKundum <SatyaKundum@gmail.com>
2019-02-15 08:10:37 +0530

    RestFullCrudService

---
+++ RestFullCrudTest/RestFullCrudTest/src/test/java/com/rest/crudtest/RestFullCrudTest/RestFullCrudTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.rest.crudtest.RestFullCrudTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestFullCrudTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
