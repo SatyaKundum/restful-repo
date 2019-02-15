66dda2e090d7d34ee405e5fa87174a098f8d681a
SatyaKundum <SatyaKundum@gmail.com>
2019-02-15 09:32:00 +0530

    Added new controller

---
+++ RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/HelloController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import java.awt.PageAttributes.MediaType;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/hellouser")
+public class HelloController {
+
+	@RequestMapping("")
+	@ResponseBody
+	public String hello() {
+		return "Welcome to Restful test";
+	}
+	
+	@RequestMapping(value="",method=RequestMethod.POST)
+	@ResponseBody
+	public String greeting(@RequestBody String name) {
+		return "Welcome to Restful test - "+name;
+	}
+}
