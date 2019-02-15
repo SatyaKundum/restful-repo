399954ecbc5b028a1891623be765d196ee5042bb
SatyaKundum <SatyaKundum@gmail.com>
2019-02-15 07:47:06 +0530

    Updating Rest Controller with annotations

--- RestFullTest/RestFullTest/src/main/java/com/rest/test/RestFullTest/RestFullTestApplication.java
+++ RestFullTest/RestFullTest/src/main/java/com/rest/test/RestFullTest/RestFullTestApplication.java
@@ -1,10 +1,23 @@
 package com.rest.test.RestFullTest;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@EnableAutoConfiguration
+@Controller
 @SpringBootApplication
+@RequestMapping("/hello")
 public class RestFullTestApplication {
+	
+	@RequestMapping("")
+	@ResponseBody
+	public String hello() {
+		return "Hello RESTFul World";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestFullTestApplication.class, args);
