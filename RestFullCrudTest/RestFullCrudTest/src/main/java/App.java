90bc32b83ac99f459dfacca49f26f06f65f7c548
SatyaKundum <SatyaKundum@gmail.com>
2019-02-16 06:10:31 +0530

    Adding Exception classe

--- RestFullCrudTest/RestFullCrudTest/src/main/java/App.java
+++ RestFullCrudTest/RestFullCrudTest/src/main/java/App.java
@@ -2,8 +2,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import com.mangofactory.swagger.plugin.EnableSwagger;
+
 @EnableAutoConfiguration
 @ComponentScan("controllers")
+@EnableSwagger
 public class App {
 	
 	public static void main(String[] args) {
--- RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/CustomerController.java
+++ RestFullCrudTest/RestFullCrudTest/src/main/java/controllers/CustomerController.java
@@ -10,57 +10,56 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import exception.CustomerNotFoundException;
 import model.Customer;
 
 @Controller
 @RequestMapping("/customers")
 public class CustomerController {
-	
+
 	private static List<Customer> list = new ArrayList<Customer>();
-	
+
 	static {
 		list.add(new Customer(1l, "Satya"));
 		list.add(new Customer(2l, "Mouni"));
 	}
-	
-	@RequestMapping(value="", method=RequestMethod.GET)
+
+	@RequestMapping(value = "", method = RequestMethod.GET)
 	@ResponseBody
-	public List<Customer> getAllCustomers(){
+	public List<Customer> getAllCustomers() {
 		return list;
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	@ResponseBody
 	public Customer getCustomer(@PathVariable("id") Long id) {
 		for (Customer c : list) {
-			if(c.getId().equals(id)) {
+			if (c.getId().equals(id)) {
 				return c;
 			}
 		}
-		return null;
+		throw new CustomerNotFoundException();
 	}
-	
-	@RequestMapping(value="", method=RequestMethod.POST)
+
+	@RequestMapping(value = "", method = RequestMethod.POST)
 	@ResponseBody
 	public void addCustomer(@RequestBody Customer customer) {
 		list.add(customer);
 	}
-	
-	@RequestMapping(value= "/{id}", method=RequestMethod.DELETE)
-	public String deleteCustomer(@PathVariable("id") Long id) {
-		String message = "";
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public void deleteCustomer(@PathVariable("id") Long id) {
 		Customer customer = null;
 		for (Customer c : list) {
-			if(c.getId().equals(id)) {
+			if (c.getId().equals(id)) {
 				customer = c;
 			}
 		}
-		if(customer == null) {
-			message = "Customer Not Found";
-		}else {
+		if (customer == null) {
+			throw new CustomerNotFoundException();
+		} else {
 			list.remove(customer);
 		}
-		return message;
 	}
 
 }
